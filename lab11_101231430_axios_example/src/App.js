b9fa1a2f9f01429a01942eaab62af8e62693f589
Vinayaka98 <67480456+Vinayaka98@users.noreply.github.com>
2022-11-20 23:04:09 -0500

    Add files via upload
    Added successful

---
+++ lab11_101231430_axios_example/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PersonList from './PersonList';
+
+function App() {
+  return (
+    <div>
+      <>
+      <h1>User List</h1>
+      <PersonList />
+      </>
+    </div>
+  );
+}
+
+export default App;
